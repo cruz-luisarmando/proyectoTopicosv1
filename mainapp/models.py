d53fc7d62e6699faede7af004a2b7bff07100aeb
cruz-luisarmando <52083491+cruz-luisarmando@users.noreply.github.com>
2019-11-10 11:18:21 -0600

    primeros cambios
    urls

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Paso(models.Model):
+    nombre_paso = models.CharField(max_length=50)
+    descripcion_paso =models.TextField()
+
+    def __str__(self):
+        return self.nombre_paso
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request=request,
+                  template_name='index.html',
+                  context=None)
+
+def homepage(request):
+    return render(request=request,
+                  template_name='homepage.html',
+                  context=None)
